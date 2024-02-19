db97d35026e1bb374ab302a364f2924ba7a4876c
Tristan Gabl <trgabl@student.ethz.ch>
2024-02-19 17:34:37 +0100

    update ex1

--- slurm_job_two.sh
+++ slurm_job_two.sh
@@ -17,3 +17,7 @@ lscpu | grep "Model name"
 
 # run (srun: run job on cluster with provided resources/allocation)
 srun hostname
+sbatch --constraint EPYC_9654 --wrap "./hello_world"
+sbatch --constraint EPYC_7763 --wrap "./hello_world"
+
+
