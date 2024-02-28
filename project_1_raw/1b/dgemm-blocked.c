cd0d426b95cda3bf24573349e97e1ef69926d9b7
Tristan Gabl <trgabl@student.ethz.ch>
2024-02-28 10:17:14 +0100

    update ex1

--- project_1_raw/1b/dgemm-blocked.c
+++ project_1_raw/1b/dgemm-blocked.c
@@ -10,10 +10,26 @@ const char* dgemm_desc = "Blocked dgemm.";
 void square_dgemm(int n, double* A, double* B, double* C) {
   // TODO: Implement the blocking optimization
   //       (The following is a placeholder naive three-loop dgemm)
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      for(int k = 0; k < n; ++k) {
-	C[i+j*n] += A[i+k*n] * B[k+j*n];
+  int s = 1000; // blocking with s-by-s matrix
+  int block = n / s; // block iterations
+  for (int i = 0; i < s; i += block) {
+    for (int j = 0; j < s; j += block) {
+      for (int k = 0; k < s; k += block) {
+        for (int ii = i; ii < i + block; ++ii) {
+          for (int jj = j; jj < j + block; ++jj) {
+            for (int kk = k; kk < k + block; ++kk){
+              C[ii+jj*n] += A[ii+kk*n] * B[kk+jj*n];
+              
+            }
+          }
+        }
+      }
+    }
+  }
+  for (int i = block * s; i < n; ++i) {
+    for (int j = block * s; j < n; ++j) {
+      for (int k = block * s; k < n; ++k) {
+        C[i+j*n] += A[i+k*n] * B[k+j*n];
       }
     }
   }
