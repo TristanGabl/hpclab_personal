e973f5ee5143c3b655f8a452bcee0ab006445f96
Tristan Gabl <trgabl@student.ethz.ch>
2024-03-04 00:04:31 +0100

    update ex1

--- project_1_raw/1b/dgemm-blocked.c
+++ project_1_raw/1b/dgemm-blocked.c
@@ -1,5 +1,8 @@
 const char* dgemm_desc = "Blocked dgemm.";
 
+#include <stdio.h>
+#include <math.h>
+
 /* This routine performs a dgemm operation
  *
  *  C := C + A * B
@@ -7,29 +10,43 @@ const char* dgemm_desc = "Blocked dgemm.";
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values.
  */
+
+
+
+#define MIN(A, B) (((A) < (B)) ? (A) : (B))
+
 void square_dgemm(int n, double* A, double* B, double* C) {
   // TODO: Implement the blocking optimization
   //       (The following is a placeholder naive three-loop dgemm)
-  int s = 1000; // blocking with s-by-s matrix
-  int block = n / s; // block iterations
-  for (int i = 0; i < s; i += block) {
-    for (int j = 0; j < s; j += block) {
-      for (int k = 0; k < s; k += block) {
-        for (int ii = i; ii < i + block; ++ii) {
-          for (int jj = j; jj < j + block; ++jj) {
-            for (int kk = k; kk < k + block; ++kk){
-              C[ii+jj*n] += A[ii+kk*n] * B[kk+jj*n];
-              
+
+  // long int l2 = 1 << 29;
+  // int s = sqrt(l2 / 3 / sizeof(double));
+  int s = 100; 
+  
+  for (int i = 0; i < n; i += s) {
+    for (int j = 0; j < n; j += s) {
+      for (int k = 0; k < n; k += s) {
+        for (int ii = i; ii < MIN(i + s, n); ++ii) {
+          for (int jj = j; jj < MIN(j + s, n); ++jj) {
+            double tmp = 0.0;
+            int kk_rest = k;
+            for (int kk = k; kk < MIN(k + s, n) - 8; kk += 8) {
+              tmp += A[ii+kk*n] * B[kk+jj*n];
+              tmp += A[ii+(kk+1)*n] * B[kk+1+jj*n];
+              tmp += A[ii+(kk+2)*n] * B[kk+2+jj*n];
+              tmp += A[ii+(kk+3)*n] * B[kk+3+jj*n];
+              tmp += A[ii+(kk+4)*n] * B[kk+4+jj*n];
+              tmp += A[ii+(kk+5)*n] * B[kk+5+jj*n];
+              tmp += A[ii+(kk+6)*n] * B[kk+6+jj*n];
+              tmp += A[ii+(kk+7)*n] * B[kk+7+jj*n];
+              kk_rest = kk + 8;
             }
+            for (int kk = kk_rest; kk < MIN(k + s, n); ++kk) {
+              tmp += A[ii+kk*n] * B[kk+jj*n];
+            }
+            C[ii+jj*n] += tmp;
           }
         }
-      }
-    }
-  }
-  for (int i = block * s; i < n; ++i) {
-    for (int j = block * s; j < n; ++j) {
-      for (int k = block * s; k < n; ++k) {
-        C[i+j*n] += A[i+k*n] * B[k+j*n];
       }
     }
   }
