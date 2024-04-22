19914b00129c577b0aa9a8e643ef4c693645d9ab
Tristan Gabl <trgabl@student.ethz.ch>
2024-04-22 10:31:55 +0200

    update p4

--- project_4_raw/ring/ring_sum.c
+++ project_4_raw/ring/ring_sum.c
@@ -1,6 +1,20 @@
 #include <mpi.h> // MPI
 #include <stdio.h>
 
+int ring_iteration(int n_ranks, int rank, int* curr_value, int* prev_value, int* sum, MPI_Status* status) {
+  int rank_next = (rank + 1) % n_ranks;
+  int rank_prev = rank == 0 ? n_ranks - 1 : rank - 1;
+
+  printf("%d: current sum = %d\n", rank, *sum);
+  MPI_Send((void *) curr_value, 1, MPI_INT, rank_next, 1, MPI_COMM_WORLD);
+
+  printf("%d: sending value %d to rank %d\n", rank, *curr_value, rank_next);
+  MPI_Recv((void *) prev_value, 1, MPI_INT, rank_prev, 1, MPI_COMM_WORLD, status);
+  
+  *sum += *prev_value;
+  *curr_value = *prev_value;
+}
+
 int main(int argc, char *argv[]) {
 
   // Initialize MPI, get size and rank
@@ -10,8 +24,15 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   // IMPLEMENT: Ring sum algorithm
-  int sum = 0; // initialize sum
-  printf("Process %i: Sum = %i\n", rank, sum);
+  int sum = 0; 
+  int curr_value = rank;
+  int prev_value;
+
+  MPI_Status status;
+  for (int i = 0; i < size; i++)
+    ring_iteration(size, rank, &curr_value, &prev_value, &sum, &status);
+
+  printf("Process %i, Comm_size %i: Sum = %i\n", rank, size, sum);
 
   // Finalize MPI
   MPI_Finalize();
