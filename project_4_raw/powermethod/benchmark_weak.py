40acbd4b4bd8ed4b663178aca8fdfa197d2f5f1b
Tristan Gabl <trgabl@student.ethz.ch>
2024-04-24 15:03:23 +0200

    update p4

---
+++ project_4_raw/powermethod/benchmark_weak.py
@@ -0,0 +1,57 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import subprocess
+from typing import List
+
+
+def run(np: int, n_x: int, n_t: int) -> float:
+    # we call subprocess with the desired variables
+    # mpirun -np np ./powermethod_rows 3 n_x n_t -1e-6
+    output = subprocess.check_output(
+        ["mpirun", "-np", f"{np}", "./powermethod_rows", "3", f"{n_x}", f"{n_t}", "-1e-6"],
+    ).decode("utf-8")
+    
+    # extract the time
+    time = float(output.split()[-2])
+    threads = int((output.split()[-6]).rstrip(","))
+    print(f"np = {threads}; time = {time}")
+    return time
+
+
+def main():
+
+    threads = [1,2,4,8,16,32,64]
+    n_x = 1000
+    n_t = 100
+
+    iterations = 1
+    iterations_warm_up = 1
+
+    time_seq = 0
+    for itr in range(iterations_warm_up):
+        run(threads[0], n_x, n_t)
+    for itr in range(iterations):
+        time_seq += run(threads[0], n_x, n_t) / iterations
+
+    efficiency  = [1.0]
+    for i in range(1, len(threads)):
+        times_par = []
+        for itr in range(iterations_warm_up):
+            run(threads[i], int(n_x * np.sqrt(threads[i])), n_t)
+        for itr in range(iterations):
+            times_par.append(run(threads[i], int(n_x * np.sqrt(threads[i])), n_t))
+
+        efficiency.append(time_seq * iterations / sum(times_par))
+
+    plt.plot(threads, efficiency)
+
+    plt.xlabel("threads")
+    plt.ylabel("efficiency")
+    plt.savefig("powermethod_benchmark_weak.png")
+    
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ project_4_raw/powermethod/run_benchmark_strong copy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=benchmark_strong      # Job name    (default: sbatch)
+#SBATCH --output=sbatch/benchmark_strong-%j.out # Output file (default: slurm-%j.out)
+#SBATCH --error=sbatch/benchmark_strong-%j.err  # Error file  (default: slurm-%j.out)
+#SBATCH --ntasks=64                # Number of tasks
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks-per-node=64       # Number of tasks per node
+#SBATCH --constraint=EPYC_7763    # Select node with CPU
+#SBATCH --cpus-per-task=1       # Number of CPUs per task
+#SBATCH --mem-per-cpu=1024        # Memory per CPU
+#SBATCH --time=04:00:00           # Wall clock time limit
+
+# Load some modules & list loaded modules
+module load gcc openmpi python
+
+python benchmark_strong.py
+echo "run_benchmark_storng done!"
+
