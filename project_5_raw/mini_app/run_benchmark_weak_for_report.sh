65e5ab59067379dc54142f2144f3d5aa6ceb0a74
Tristan Gabl <trgabl@student.ethz.ch>
2024-05-11 00:18:40 +0200

    update p5

--- project_5_raw/mini_app/run_benchmark_weak_for_report.sh
+++ project_5_raw/mini_app/run_benchmark_weak_for_report.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=benchmark_weak      # Job name    (default: sbatch)
 #SBATCH --output=build/benchmark_weak-%j.out # Output file (default: slurm-%j.out)
 #SBATCH --error=build/benchmark_weak-%j.err  # Error file  (default: slurm-%j.out)
-#SBATCH --ntasks=16                # Number of tasks
+#SBATCH --ntasks=64                # Number of tasks
 #SBATCH --constraint=EPYC_7763    # Select node with CPU
 #SBATCH --cpus-per-task=1      # Number of CPUs per task
 #SBATCH --mem-per-cpu=1024        # Memory per CPU
